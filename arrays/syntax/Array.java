ecb8508afe655d38d1278b2a9dbf0385045c3621
cubewin <tanthang071208@gmail.com>
2026-03-12 09:05:38 +1300

    Moving review out to main folder + create systax folder for array

---
+++ arrays/syntax/Array.java
@@ -0,0 +1,29 @@
+package arrays.syntax;
+
+public class Array {
+    public static void main(String[] args) {
+        // 4 ways to init an array
+        int[] a = new int[5];
+        int b[] = new int[5];
+        int[] c = {1,2,3,4,5};
+        int[] d = new int[]{1,2,3,4,5};
+
+        // Time complexity 
+            // Access - O(1)
+            // Insert(middle) - O(n)
+            // Update - O(n)
+            // Delete - O(n)
+            // Search(unsorted) - O(n)
+
+        // Common loop
+        for(int i=0; i<a.length; i++){
+            System.out.println(a[i]);
+            a[i] = 2;
+        }
+
+        // Enhanced loop
+        for(int num : b){
+            System.out.println(num);
+        }
+    }
+}
--- review.java
+++ review.java
@@ -1,4 +1,4 @@
-package syntax;
+
 
 import java.util.Scanner;
 
