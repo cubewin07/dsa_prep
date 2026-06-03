a84d8bccfa2dc19146c7c8271fbd0504c6dcb93b
cubewin <tanthang071208@gmail.com>
2026-06-04 10:12:19 +1200

    Rename class name of version of remove duplicates from sorted array + getting rid of unused variable

--- Problems/Remove_duplicates_from_sorted_array/Remove_duplicates_from_sorted_array.java
+++ Problems/Remove_duplicates_from_sorted_array/Remove_duplicates_from_sorted_array.java
@@ -1,8 +1,8 @@
 package Problems.Remove_duplicates_from_sorted_array;
 import java.util.HashSet;
-class Solution {
+
+public class Remove_duplicates_from_sorted_array {
     public int removeDuplicates(int[] nums) {
-        int k = 0;
         int left=0;
         HashSet<Integer> set = new HashSet<>();
         for(int i = 0; i<nums.length; i++){
